62f55db60e80c825c3c37f78f9f20127f9a4af18
Quantum cat <lkstc112233@users.noreply.github.com>
2018-01-20 12:58:20 -0500

    Clock Register toString

--- src/increment/simulator/ClockRegister.java
+++ src/increment/simulator/ClockRegister.java
@@ -1,5 +1,17 @@
 package increment.simulator;
 
+/**
+ * A clock register.
+ * 
+ * A clock register will have two inputs:
+ * 		* write[1]
+ * 		* input[width]
+ * A clock register will have one output:
+ * 		* output[width]
+ * 
+ * @author lkstc
+ *
+ */
 public class ClockRegister extends Chip {
 	// I'm using a Cable class for storage here. It's just because it fits here.
 	protected Cable data;
@@ -31,5 +43,14 @@ public class ClockRegister extends Chip {
 	 */
 	public void evaluate(){
 		outputs.get("output").assign(data);
+	}
+	/**
+	 * Turns chip value into a readable way.
+	 */
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Reg: ");
+		sb.append(data.toInteger());
+		return sb.toString();
 	}
 }
