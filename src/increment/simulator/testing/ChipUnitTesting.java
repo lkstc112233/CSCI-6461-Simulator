a117b0e060535dd9e36ae7d8756d5b8e1902c81f
Quantum cat <lkstc112233@users.noreply.github.com>
2018-01-23 03:32:03 -0500

    Unit Testing Hub.

---
+++ src/increment/simulator/testing/ChipUnitTesting.java
@@ -0,0 +1,16 @@
+package increment.simulator.testing;
+
+
+/** 
+ * This class is intended to handle unit testing for chip (somehow). 
+ * @author Xu Ke
+ *
+ */
+public class ChipUnitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
