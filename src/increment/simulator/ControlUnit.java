9eb3cac588b739edb5f3c7faded8b5fa38776159
Photon Cat <lkstc112233@gmail.com>
2018-01-23 20:19:17 -0500

    A naive control unit.

---
+++ src/increment/simulator/ControlUnit.java
@@ -0,0 +1,64 @@
+package increment.simulator;
+
+import java.util.Map.Entry;
+
+/**
+ * The control unit. It controls how everything else works, such as write signals, or who is to use the bus.
+ * 
+ * The controlUnit has one input:
+ * 		* opcode[6]
+ * 
+ * And it has several outputs connecting to every part in the CPU chip.
+ * 
+ * @author Xu Ke
+ *
+ */
+public class ControlUnit extends Chip {
+	/**
+	 * Indicating the current processor status.
+	 * Member format:
+	 * 		StateName_PhaseName, all caps.
+	 * @author Xu Ke
+	 *
+	 */
+	private enum Status{
+		FETCH_PC_TO_MAR,
+		FETCH_MEMORY_ACCESS,
+		FETCH_MBR_TO_IR,
+	}
+	Status status;
+	public ControlUnit() {
+		status = Status.FETCH_PC_TO_MAR;
+		addInput("opcode", 6);
+		addOutput("PC_write", 1);
+		addOutput("PC_output", 1);
+		addOutput("MAR_write", 1);
+		addOutput("memory_read", 1);
+		addOutput("memory_write", 1);
+		addOutput("register_write", 1);
+	}
+	/**
+	 * Resets all outputs to zero.
+	 * So after calling this, we only have to set those should be 1 to 1.
+	 */
+	protected void resetOutputs(){
+		for (Entry<String, Cable> c : outputs.entrySet()){
+			c.getValue().setZero();
+		}
+	}
+	// The control Unit will perform an action based on current status every tick.
+	public void evaluate(){
+		resetOutputs();
+		switch (status){
+		case FETCH_PC_TO_MAR:
+			outputs.get("PC_output").putValue(1);
+			outputs.get("MAR_write").putValue(1);
+			status = Status.FETCH_MEMORY_ACCESS;
+			break;
+		case FETCH_MEMORY_ACCESS:
+			break;
+		case FETCH_MBR_TO_IR:
+			break;
+		}
+	}
+}
