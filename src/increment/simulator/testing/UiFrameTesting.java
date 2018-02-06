a5b174f5fa74f7a7a808e698042d575d886a0e40
Photon Cat <lkstc112233@gmail.com>
2018-02-06 17:05:52 -0500

    get rid of warnings.

--- src/increment/simulator/testing/UiFrameTesting.java
+++ src/increment/simulator/testing/UiFrameTesting.java
@@ -1,32 +1,19 @@
 package increment.simulator.testing;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import increment.simulator.Machine;
 import increment.simulator.SingleCable;
 import increment.simulator.front_panel.BulbSet;
 import increment.simulator.front_panel.SwitchesSet;
-import increment.simulator.ui.MachineWrapper;
 import javafx.application.Application;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class UiFrameTesting extends Application {
