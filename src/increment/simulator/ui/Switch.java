a6b70293dc95bb106fefcbc19587fcf3e7ab6557
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-08 01:48:43 -0500

    re add the UI part

---
+++ src/increment/simulator/ui/Switch.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+
+public class Switch {
+
+  Image image1 = new Image("increment/simulator/close2.png");
+  Image image2 = new Image("increment/simulator/open.png");
+ public ImageView imageView =new ImageView(image1);
+ public boolean IsOpen = false;
+
+
+    public Switch(int Height, int Width) {
+
+        imageView.setFitHeight(Height);
+        imageView.setFitWidth(Width);
+
+         imageView.setOnMousePressed(event -> {
+
+             if(!IsOpen)
+        {
+            imageView.setImage(image2);
+            IsOpen=true;
+        }
+        else {
+            imageView.setImage(image1);
+             IsOpen=false;
+        }
+
+         });
+
+    }
+
+    public Image getImage(){
+
+        return imageView.getImage();
+    }
+
+}
+
+
+
+
