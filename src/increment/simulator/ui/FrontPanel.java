b6651fdd970fd68e18901428f5755b132d8efff8
Photon Cat <lkstc112233@gmail.com>
2018-02-08 16:27:58 -0500

    Load into mar or memory.
    When load into memory, mar is added by 1.

--- src/increment/simulator/ui/FrontPanel.java
+++ src/increment/simulator/ui/FrontPanel.java
@@ -10,7 +10,9 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class FrontPanel extends Stage {
@@ -27,14 +29,14 @@ public class FrontPanel extends Stage {
 			grid.getColumnConstraints().add(cc);
 		}
 		RowConstraints rc = new RowConstraints();
-		for (int j = 0; j < 3; ++j) {
-			rc = new RowConstraints();
-			rc.setPercentHeight(30 / 3.);
-			grid.getRowConstraints().add(rc);
-		}
 		for (int j = 0; j < 5; ++j) {
 			rc = new RowConstraints();
-			rc.setPercentHeight(70 / 5.);
+			rc.setPercentHeight(40 / 5.);
+			grid.getRowConstraints().add(rc);
+		}
+		for (int j = 0; j < 4; ++j) {
+			rc = new RowConstraints();
+			rc.setPercentHeight(60 / 4.);
 			grid.getRowConstraints().add(rc);
 		}
 		grid.setHgap(50);
@@ -49,17 +51,23 @@ public class FrontPanel extends Stage {
 			if (i < 12) {
 				radio.setDisable(true);
 				radio.selectedProperty().bind(machine.getAddressBulbsProperty(i));
-				grid.add(radio, i, 0);
+				grid.add(radio, i, 1);
 			}
 			radio = new RadioButton();
 			radio.setDisable(true);
 			radio.selectedProperty().bind(machine.getValueBulbsProperty(i));
-			grid.add(radio, i, 1);
+			grid.add(radio, i, 2);
 			CheckBox check = new CheckBox();
 			machine.getSwitchesProperty(i).bind(check.selectedProperty());
-			grid.add(check, i, 2);
+			grid.add(check, i, 3);
+			grid.add(new Text(Integer.toString(i)), i, 0);
+			grid.add(new Text(Integer.toString(i)), i, 4);
 		}
 
+		HBox box = new HBox();
+		box.setSpacing(10);
+		grid.add(box, 0, 5, 17, 1);
+		
 		Button button = new Button("Tick");
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -67,7 +75,7 @@ public class FrontPanel extends Stage {
 				machine.forceTick();
 			}
 		});
-		grid.add(button, 0, 4, 2, 1);
+		box.getChildren().add(button);
 		button = new Button("Reset CU Status");
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -75,7 +83,7 @@ public class FrontPanel extends Stage {
 				machine.resetCUStatus();
 			}
 		});
-		grid.add(button, 2, 4, 3, 1);
+		box.getChildren().add(button);
 		button = new Button("Load PC");
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -83,6 +91,22 @@ public class FrontPanel extends Stage {
 				machine.forceLoadPC();
 			}
 		});
-		grid.add(button, 5, 4, 3, 1);
+		box.getChildren().add(button);
+		button = new Button("Load MAR");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				machine.forceLoadMAR();
+			}
+		});
+		box.getChildren().add(button);
+		button = new Button("Load Data into Memory");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				machine.loadDataIntoMemory();
+			}
+		});
+		box.getChildren().add(button);
 	}
 }
