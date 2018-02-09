b5cb1fac5cd83be2c2f91769f9516209e5c4f649
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-08 21:15:30 -0500

    CSS changes

--- src/increment/simulator/ui/Switchbutton.java
+++ src/increment/simulator/ui/Switchbutton.java
@@ -1,17 +1,26 @@
-package sample;
+package increment.simulator.ui;
 
+
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+import javafx.scene.Node;
 import javafx.scene.image.*;
 import javafx.scene.image.Image;
 
-public class Switch {
+import java.awt.*;
 
-  Image image1 = new Image("increment/simulator/close2.png");
-  Image image2 = new Image("increment/simulator/open.png");
+public class Switchbutton extends Frame {
+
+  Image image1 = new Image("file:res/close2.png");
+  Image image2 = new Image("file:res/open.png");
  public ImageView imageView =new ImageView(image1);
  public boolean IsOpen = false;
 
 
-    public Switch(int Height, int Width) {
+    public Switchbutton(int Height, int Width) {
 
         imageView.setFitHeight(Height);
         imageView.setFitWidth(Width);
@@ -36,6 +45,7 @@ public class Switch {
 
         return imageView.getImage();
     }
+
 
 }
 
