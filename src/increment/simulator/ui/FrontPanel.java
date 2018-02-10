1a3c5b8ab883c945626923d93a0c9656f0f0796c
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-10 02:11:07 -0500

    temperary changing for test of UI

--- src/increment/simulator/ui/FrontPanel.java
+++ src/increment/simulator/ui/FrontPanel.java
@@ -2,11 +2,14 @@ package increment.simulator.ui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -42,27 +45,41 @@ public class FrontPanel extends Stage {
 		grid.setHgap(50);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-
-		Scene scene = new Scene(grid, 600, 210);
-		scene.getStylesheets().add("increment/simulator/testing/buttonstyle.css");
+	    /*  this instruction is used to adjust the place of the children of grid, delimited when not need
+	     *
+	     *
+	     */
+		grid.setGridLinesVisible(true);
+		Scene scene = new Scene(grid, 600, 400);
+		scene.getStylesheets().add("increment/simulator/ui/buttonstyle.css");
 		setScene(scene);
-		
+		/* centerlize  the node */
+		grid.setAlignment(Pos.CENTER);
+
+
+
 		for (int i = 0; i < 16; ++i) {
 			RadioButton radio = new RadioButton();
+
 			if (i < 12) {
 				radio.setDisable(true);
 				radio.selectedProperty().bind(machine.getAddressBulbsProperty(i));
 				grid.add(radio, i, 1);
+				grid.setHalignment(radio, HPos.CENTER);
 			}
 			radio = new RadioButton();
 			radio.setDisable(true);
 			radio.selectedProperty().bind(machine.getValueBulbsProperty(i));
 			grid.add(radio, i, 2);
+			grid.setHalignment(radio, HPos.CENTER);
 			CheckBox check = new CheckBox();
 			machine.getSwitchesProperty(i).bind(check.selectedProperty());
-			grid.add(check, i, 3);
-			grid.add(new Text(Integer.toString(i)), i, 0);
-			grid.add(new Text(Integer.toString(i)), i, 4);
+			Text text1 = new Text(Integer.toString(i));
+			Text text2 = new Text(Integer.toString(i));
+			grid.setHalignment(text1, HPos.CENTER);
+			grid.add(text1, i, 0);
+			grid.setConstraints(text2,i,4,1,1, HPos.CENTER, VPos.CENTER);
+			grid.add(text2, i, 4);
 		}
 
 		HBox box = new HBox();
@@ -70,6 +87,12 @@ public class FrontPanel extends Stage {
 		grid.add(box, 0, 5, 17, 1);
 		
 		Button button = new Button("Tick");
+		/*test for single button work */
+		button.setStyle("  -fx-background-color:\n" +
+				"        #3c7fb1,\n" +
+				"        linear-gradient(#fafdfe, #e8f5fc),\n" +
+				"        linear-gradient(#eaf6fd 0%, #d9f0fc 49%, #bee6fd 50%, #a7d9f5 100%);");
+		//grid.halignmentProperty();
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
