fd7b1463e5832c61d3fd0b86277bb46bdc0cf65a
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-22 22:30:02 -0500

    bind the two button pause and circle also the pause radio
    bind the two button pause and circle also the pause radio

--- src/increment/simulator/userInterface/FrontPanelController.java
+++ src/increment/simulator/userInterface/FrontPanelController.java
@@ -2,21 +2,30 @@ package increment.simulator.userInterface;
 
 import increment.simulator.ui.MachineWrapper;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class FrontPanelController {
 	private MachineWrapper machine;
+	@FXML private RadioButton Radio_Pause;
+
 	public void setMachine(MachineWrapper machine) {
 		this.machine = machine;
+        Radio_Pause.selectedProperty().bind(this.machine.getPausedProperty());
 	}
 
     public void handleCircleButtonAction(ActionEvent actionEvent) {
+        machine.forceTick();
     }
 
     public void handlePauseButtonAction(ActionEvent actionEvent) {
+        machine.pauseOrRestore();
     }
 
     public void handleResetCUButtonAction(ActionEvent actionEvent) {
