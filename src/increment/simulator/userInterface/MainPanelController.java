0b62d8550e64548da589c3bf7cf0ff46fb0506b2
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-22 21:29:55 -0500

    Binding of mainpanel
    1: changed the name of  Bus_Text which once was PC_Text1 that is not correct.(in mainpanel fxml)
    2:add new textbox to the Scollpanel when the  memory output is different from others at newMain settings.
    3.add the bind to the mainpanel which is the debug panel.

--- src/increment/simulator/userInterface/MainPanelController.java
+++ src/increment/simulator/userInterface/MainPanelController.java
@@ -14,11 +14,30 @@ import javafx.scene.text.Text;
 
 public class MainPanelController {
 	public void setMachine(MachineWrapper machine) {
-		Text_PC_Text.textProperty().bind(machine.getProgramCounterProperty());
+
+		Text_Tick_Text.textProperty().bind(machine.getTickProperty().asString());
+        Text_PC_Text.textProperty().bind(machine.getProgramCounterProperty());
+		Text_BUS_Text.textProperty().bind(machine.getBusProperty());
+		Text_MAR_Text.textProperty().bind(machine.getMemoryAddressRegisterProperty());
+		Text_MBR_Text.textProperty().bind(machine.getMemoryBufferRegisterProperty());
+		Text_IR_Text.textProperty().bind(machine.getInstructionRegisterProperty());
+		Text_GPRF_Text.textProperty().bind(machine.getGeneralPurposeRegisterFileProperty());
+		Text_IRF_Text.textProperty().bind(machine.getIndexRegisterFileProperty());
+		Text_CU_Text.textProperty().bind(machine.getControlUnitProperty());
+		Text_Memory_Text.textProperty().bind(machine.getMemoryProperty());
+
 	}
-	
-    @FXML private Text Text_PC_Text;
-    @FXML private  Text Text_PC_Label;
+
+    @FXML private  Text Text_Tick_Text;
+    @FXML private  Text Text_PC_Text;
+    @FXML private  Text Text_BUS_Text;
+    @FXML private  Text Text_MAR_Text;
+    @FXML private  Text Text_MBR_Text;
+    @FXML private  Text Text_IR_Text;
+    @FXML private  Text Text_GPRF_Text;
+    @FXML private  Text Text_IRF_Text;
+    @FXML private  Text Text_CU_Text;
+    @FXML private  Text Text_Memory_Text;
 
     public void handleTickButtonAction(ActionEvent actionEvent) {
 
