b04c1c132f3cb46748fcf70c8cf0c97cfb068940
Quantum cat <lkstc112233@users.noreply.github.com>
2018-02-25 18:46:19 -0500

    Adder and AU change for adding two numbers

--- src/increment/simulator/chips/ArithmeticUnit.java
+++ src/increment/simulator/chips/ArithmeticUnit.java
@@ -1,5 +1,9 @@
 package increment.simulator.chips;
 
+import increment.simulator.Cable;
+import increment.simulator.CablePartialAdapter;
+import increment.simulator.SingleCable;
+
 /**
  * The arithmetic unit. Inside an ALU probably.
  * 
@@ -14,18 +18,39 @@ package increment.simulator.chips;
  * @author Xu Ke
  *
  */
-public class ArithmeticUnit extends Chip {
+public class ArithmeticUnit extends ChipsSet {
+	CablePartialAdapter conditionAdapter;
 	public ArithmeticUnit(int width) {
 		addPort("operand1", width);
 		addPort("operand2", width);
 		addPort("result", width);
 		addPort("opcode", 6);
 		addPort("condition", 4);
+		
+		Mux outputMux = new Mux(6, width);
+		addChip(outputMux);
+		addChipPortRelation("result", outputMux, "output");
+		
+		Adder adder = new Adder(width);
+		addChip(adder);
+		addChipPortRelation("operand1", adder, "operand1");
+		addChipPortRelation("operand2", adder, "operand2");
+		SingleCable cable = new SingleCable(width);
+		
+		adder.connectPort("result", cable);
+		outputMux.connectPort("input4", cable);
+		outputMux.connectPort("input5", cable);
+		outputMux.connectPort("input6", cable);
+		outputMux.connectPort("input7", cable);
+		
+		conditionAdapter = new CablePartialAdapter(4, getPort("condition"));
+		adder.connectPort("overflow", new CablePartialAdapter(1, conditionAdapter, 0));
+		adder.connectPort("underflow", new CablePartialAdapter(1, conditionAdapter, 1));
+		
 	}
-	
 	@Override
-	public boolean evaluate() {
-		// TODO: put everything here.
-		return false;
+	public void connectPort(String name, Cable cable) {
+		super.connectPort(name, cable);
+		conditionAdapter.reMother(getPort("condition"));
 	}
 }
