524763054de59b0524de155fb285adb8230c28ae
Fergusshifairlove <fergusshi47@gmail.com>
2018-02-25 17:31:34 -0500

    bug fix

--- src/increment/simulator/userInterface/FrontPanelController.java
+++ src/increment/simulator/userInterface/FrontPanelController.java
@@ -42,7 +42,11 @@ public class FrontPanelController {
         machine.getSwitchesProperty(i).bind(((CheckBox)FrontPanel.lookup("#Check_Switch_"+i)).selectedProperty());
 
         automaticTick = new Timeline(new KeyFrame(Duration.seconds(1), event -> machine.tick()));
+
+
         automaticTick.setCycleCount(Timeline.INDEFINITE);
+
+
 
 	}
 
@@ -78,8 +82,10 @@ public class FrontPanelController {
             duration = Duration.seconds(Slider_Auto_set.getValue());
             KeyFrame keyFrame = new KeyFrame(duration,event -> {machine.tick();});
             automaticTick.getKeyFrames().setAll(keyFrame);
+
             automaticTick.play();
 
+        }
     }
 
 
