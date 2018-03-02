564f0ad657968f28f1746b1a5f534fc0e661f55e
Photon Cat <lkstc112233@gmail.com>
2018-03-02 16:30:45 -0500

    Shifting unit finished

--- src/increment/simulator/chips/ShiftingUnit.java
+++ src/increment/simulator/chips/ShiftingUnit.java
@@ -4,9 +4,9 @@ package increment.simulator.chips;
  * The shifting unit. Inside an ALU probably.
  * 
  * The shifting unit takes 3 inputs:
- * 		* operand1[width]
- * 		* shiftingInstruction[4]
- * 		* opcode[6]
+ * 		* operand[width]
+ * 		* count[4]
+ * 		* shiftingInstruction[3]
  * The logical unit gives 1 output:
  * 		* result[width]
  * 
@@ -14,5 +14,40 @@ package increment.simulator.chips;
  *
  */
 public class ShiftingUnit extends Chip {
-
+	public ShiftingUnit(int width) {
+		addPort("operand", width);
+		addPort("shiftingInstruction", 3);
+		addPort("count", 4);
+		addPort("result", width);
+	}
+	@Override
+	public boolean evaluate() {
+		long operand = getPort("operand").toInteger();
+		int width = getPort("operand").getWidth();
+		int count = (int) getPort("count").toInteger();
+		switch((int)getPort("shiftingInstruction").toInteger()){
+		case 0:
+			return assignPort("result", (operand & (1 << (width - 1))) | ((operand & ((1 << (width - 1)) - 1)) >> count) | ((operand & ((1 << count) - 1)) << (width - 1 - count)));
+		case 1:
+			return assignPort("result", (operand & (1 << (width - 1))) | ((operand & ((1 << (width - 1)) - 1)) >> (width - 1 - count)) | ((operand & ((1 << (width - 1 - count)) - 1)) << count));
+		case 2:			
+			return assignPort("result", (operand >> count) | ((operand & ((1 << count) - 1)) << (width - count)));
+		case 3:
+			return assignPort("result", (operand >> (width - count)) | ((operand & ((1 << (width - count)) - 1)) << count));
+		case 4:
+			while (count > 0) {
+				count -= 1;
+				operand >>= 1;
+				operand |= (operand << 1) & (1 << (width - 1));
+			}
+			return assignPort("result", operand);
+		case 5:
+			return assignPort("result", operand << count);
+		case 6:
+			return assignPort("result", operand >> count);
+		case 7:
+			return assignPort("result", operand << count);
+		}
+		return false;
+	}
 }
