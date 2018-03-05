c4d8e5367d7e4fd9171c49c3043ec5a7aedfa05e
Quantum cat <lkstc112233@users.noreply.github.com>
2018-03-04 20:09:39 -0500

    Menlo font

--- src/increment/simulator/userInterface/Main.java
+++ src/increment/simulator/userInterface/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Font;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,6 +17,7 @@ public class Main extends Application {
 	
     @Override
     public void start(Stage primaryStage) throws Exception{
+    	Font.loadFont(getClass().getResource("/res/font/Menlo.ttf").toExternalForm(), 10);
     	machine = new MachineWrapper(new Machine());
     	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/res/fxml/FrontPanel.fxml"));
         Parent root = fxmlLoader.load();
