51cda783c0719d34df06f783eb731460092a69aa
Photon Cat <lkstc112233@gmail.com>
2018-04-17 20:26:21 -0400

    Remove warnings.

--- src/increment/simulator/ui/FrontPanel.java
+++ src/increment/simulator/ui/FrontPanel.java
@@ -10,7 +10,6 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.ColumnConstraints;
--- src/increment/simulator/ui/Switchbutton.java
+++ src/increment/simulator/ui/Switchbutton.java
@@ -1,12 +1,6 @@
 package increment.simulator.ui;
 
 
-import com.sun.javafx.geom.BaseBounds;
-import com.sun.javafx.geom.transform.BaseTransform;
-import com.sun.javafx.jmx.MXNodeAlgorithm;
-import com.sun.javafx.jmx.MXNodeAlgorithmContext;
-import com.sun.javafx.sg.prism.NGNode;
-import javafx.scene.Node;
 import javafx.scene.image.*;
 import javafx.scene.image.Image;
 
@@ -14,7 +8,11 @@ import java.awt.*;
 
 public class Switchbutton extends Frame {
 
-    Image image1 = new Image("file:res/close2.png");
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 319478816115107399L;
+	Image image1 = new Image("file:res/close2.png");
     Image image2 = new Image("file:res/open.png");
     public ImageView imageView =new ImageView(image1);
     public boolean IsOpen = false;
